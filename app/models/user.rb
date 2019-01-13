a4d514fab0c93dad5e49d747ac7e21106afc0038
github username <github email address>
2019-01-13 10:54:59 -0500

    Add devise to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_many  :user_disorders
   has_many  :user_disorder_treatments, through: :user_disorders
   has_many :disorders, through: :user_disorders
