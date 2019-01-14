031464f24ae275b955f19f7f706bba2a1bef41d4
github username <github email address>
2019-01-14 05:29:14 -0500

    Create /signup path

--- config/routes.rb
+++ config/routes.rb
@@ -27,5 +27,9 @@ root to: 'users#index'
   devise_scope :user do
     get 'login', to: 'devise/sessions#new'
   end
+
+  devise_scope :user do
+    get 'signup', to: 'devise/registrations#new'
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
