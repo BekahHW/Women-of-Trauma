763d72498c807540ea758305668ebbadce207847
github username <github email address>
2019-01-14 14:33:04 -0500

    Add relationships to the Disorder model

--- app/models/disorder.rb
+++ app/models/disorder.rb
@@ -1,2 +1,7 @@
 class Disorder < ApplicationRecord
+  has_many :user_disorders
+  has_many :user_disorder_treatments, through: :user_disorders
+  has_many :users, through: :user_disorders
+  has_many :treatments, through: :user_disorder_treatments
+
 end
--- app/models/user_disorder.rb
+++ app/models/user_disorder.rb
@@ -2,7 +2,6 @@ class UserDisorder < ApplicationRecord
   belongs_to :user
 	belongs_to :disorder
   belongs_to :user_disorder_treatments
-  has_many :treatments, through: :user_disorder_treatments
 
   validates :narrative, presence: true, length: { minimum: 250 }
 
