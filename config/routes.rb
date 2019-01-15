fdf26b60817657c12a3e4b2966d00efe6fa63969
github username <github email address>
2019-01-15 06:47:07 -0500

    -Create an index page for user disorder with all narratives truncated -Update routes to resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,37 @@
 Rails.application.routes.draw do
 devise_for :users, :controllers => {:registrations => "registrations"}
 root to: 'users#index'
-  get 'treatments/new'
-  get 'treatments/create'
-  get 'treatments/update'
-  get 'treatments/edit'
-  get 'treatments/show'
-  get 'user_disorders/new'
-  get 'user_disorders/create'
-  get 'user_disorders/edit'
-  get 'user_disorders/update'
-  get 'user_disorders/show'
-  get 'disorders/new'
-  get 'disorders/create'
-  get 'disorders/edit'
-  get 'disorders/update'
-  get 'disorders/show'
-  get 'users/new'
-  get 'users/index'
-  get 'users/create'
-  get 'users/edit'
-  get 'users/update'
-  get 'users/show'
-  get 'users/destroy'
+  # get 'treatments/new'
+  # get 'treatments/create'
+  # get 'treatments/update'
+  # get 'treatments/edit'
+  # get 'treatments/show'
+  # get 'user_disorders/new'
+  # get 'user_disorders/create'
+  # get 'user_disorders/edit'
+  # get 'user_disorders/update'
+  # get 'user_disorders/show'
+  # get 'disorders/new'
+  # get 'disorders/create'
+  # get 'disorders/edit'
+  # get 'disorders/update'
+  # get 'disorders/show'
+  # get 'users/new'
+  # get 'users/index'
+  # get 'users/create'
+  # get 'users/edit'
+  # get 'users/update'
+  # get 'users/show'
+  # get 'users/destroy'
+
+  resources :user_disorders
+  resources :disorders
+  resources :users
+  resources :treatments
+
+
+
+
   get 'user_disorders/index'
 
   devise_scope :user do
