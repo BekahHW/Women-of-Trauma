c2d52b7cb42094c17477589b38f4229ba4179006
github username <github email address>
2019-01-16 17:58:57 -0500

    -fill out instance var and conditions for users_controller -create user/new

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,42 @@
 class UsersController < ApplicationController
-  def index
-  end
-  
-  def new
-  end
+    def index
+      @users = User.limit(10)
+    end
 
-  def create
-  end
+    def new
+      @user = User.new
+    end
 
-  def edit
-  end
+    def create
+      @user = User.new(user_params)
+       if @user.valid?
+         @user.save
+         redirect_to user_path(@user)
+       else
+         render :new
+       end
+    end
 
-  def update
-  end
+    def edit
+      @user = User.find(params[:id])
+    end
 
-  def show
-  end
+    def update
+      @user = User.find(params[:id])
+      @user.update(params.require(:user))
+      redirect_to user_path(@user)
+    end
+
+    def show
+      @user = User.find(params[:id])
+    end
 
   def destroy
   end
+
+private
+  def user_params
+    params.require(:user).permit(:name, :location, :email, :password, :password_confirmation)
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,6 @@ class User < ApplicationRecord
   has_many  :user_disorder_treatments, through: :user_disorders
   has_many :disorders, through: :user_disorders
 
-  # has_secure_password
 
   validates :name, presence: true, uniqueness: true
 end
