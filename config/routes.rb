5f1f1fbeea0bed838c5cf2859801683e3a5040bb
github username <github email address>
2019-01-19 08:44:38 -0500

    Clean up routes code

--- config/routes.rb
+++ config/routes.rb
@@ -1,30 +1,7 @@
 Rails.application.routes.draw do
 devise_for :users, :controllers => {:registrations => "registrations"}
 root to: 'pages#index'
-  # get 'treatments/new'
-  # get 'treatments/create'
-  # get 'treatments/update'
-  # get 'treatments/edit'
-  # get 'treatments/show'
-  # get 'user_disorders/new'
-  # get 'user_disorders/create'
-  # get 'user_disorders/edit'
-  # get 'user_disorders/update'
-  # get 'user_disorders/show'
-  # get 'disorders/new'
-  # get 'disorders/create'
-  # get 'disorders/edit'
-  # get 'disorders/update'
-  # get 'disorders/show'
-  # get 'users/new'
-  # get 'users/index'
-  # get 'users/create'
-  # get 'users/edit'
-  # get 'users/update'
-  # get 'users/show'
-  # get 'users/destroy'
 
-  # resources :user_disorders
   resources :disorders
   resources :users do
     resources :user_disorders, only: [:index]
@@ -35,7 +12,7 @@ root to: 'pages#index'
 
 
 
-  get 'user_disorders/index'
+  # get 'user_disorders/index'
 
   devise_scope :user do
     get 'login', to: 'devise/sessions#new'
