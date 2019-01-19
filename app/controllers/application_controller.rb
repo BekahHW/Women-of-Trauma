60b12815e6b7205b9564ba26c3bd5cf009fc075d
github username <github email address>
2019-01-19 09:46:44 -0500

    comment out helper methods bc they were breaking login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  private
-    def current_user
-      @current_user ||= User.find_by(id: session[:user_id])
-    end
-
-    def logged_in?
-      !!current_user
-    end
+  # private
+  #   def current_user
+  #     @current_user ||= User.find_by(id: session[:user_id])
+  #   end
+  #
+  #   def logged_in?
+  #     !!current_user
+  #   end
 end
