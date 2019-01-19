fd595c084219be54a2f16e0dee0addb5f863a60c
github username <github email address>
2019-01-19 11:14:12 -0500

    Eliminate the has_many through rel for UserDisorder

--- app/models/user_disorder_treatment.rb
+++ app/models/user_disorder_treatment.rb
@@ -1,6 +1,4 @@
 class UserDisorderTreatment < ApplicationRecord
   belongs_to :user_disorder
 	belongs_to :treatment
-  has_many :disorders, through: user_disorders
-  has_many :users, through: user_disorder
 end
