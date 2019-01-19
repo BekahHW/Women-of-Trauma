27e9119d502e9b8d84ebd251779373b5a6ab896d
github username <github email address>
2019-01-19 15:08:17 -0500

    -Add content to treatments form > -Added treatment params and treatment create

--- app/controllers/treatments_controller.rb
+++ app/controllers/treatments_controller.rb
@@ -2,12 +2,19 @@ class TreatmentsController < ApplicationController
   def index
     @treatments = Treatment.all
   end
-  
+
   def new
+    @treatment = Treatment.new
   end
 
   def create
-  end
+    @treatment = Treatment.create(treatment_params)
+    if @treatment.save
+      redirect_to 'treatments_path(@treatment)'
+    else
+      render :new
+    end
+  end 
 
   def update
   end
@@ -17,4 +24,10 @@ class TreatmentsController < ApplicationController
 
   def show
   end
+
+  private
+   def treatment_params
+     params.require(:treatment).permit(:med_name, :med_dosage, :therapies, :comments)
+   end
+
 end
