64a88cccc747f09949aeb9afba585952e2eb0603
github username <github email address>
2019-01-19 10:36:36 -0500

    Comment out unneeded code in user_disorder model

--- app/models/user_disorder.rb
+++ app/models/user_disorder.rb
@@ -3,8 +3,16 @@ class UserDisorder < ApplicationRecord
 	belongs_to :disorder
   belongs_to :user_disorder_treatments
 
-  # validates :narrative, presence: true, length: { minimum: 250 }
+  validates :narrative, presence: true, length: { minimum: 250 }
 
-
+#   accepts_nested_attributes_for :disorder
+#
+# # only creating a disorder if it's not already there
+#   def disorder_attributes=(disorder_attributes)
+#    disorder_attributes.values.each do |disorder_attribute|
+#      disorder = Disorder.find_or_create_by(disorder_attribute)
+#      self.disorders << disorder
+#    end
+#  end
 
 end
