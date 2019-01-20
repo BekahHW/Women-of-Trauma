a90ee8af1f09db2ac832095595cfc7e76b6fe70f
github username <github email address>
2019-01-20 08:27:16 -0500

    Add callbacks controller with github route

--- config/routes.rb
+++ config/routes.rb
@@ -20,5 +20,7 @@ root to: 'pages#index'
   devise_scope :user do
     get 'signup', to: 'devise/registrations#new'
   end
+
+  devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
