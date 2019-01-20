abad54f8ca4eca4de400b72240a91fc57fb2abb4
github username <github email address>
2019-01-20 08:40:42 -0500

    Update routes for omniauth with devise and create migration for omniauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-devise_for :users, :controllers => {:registrations => "registrations"}
+# devise_for :users, :controllers => {:registrations => "registrations"}
 root to: 'pages#index'
 
   resources :disorders
@@ -9,18 +9,15 @@ root to: 'pages#index'
   resources :treatments
   resources :user_disorders
 
-
-
-  # get 'user_disorders/index'
-
   devise_scope :user do
     get 'login', to: 'devise/sessions#new'
-  end
-
-  devise_scope :user do
     get 'signup', to: 'devise/registrations#new'
   end
 
-  devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks", registrations: 'users/registrations' }, skip: [:sessions]
+     as :user do
+       get 'login' => 'devise/sessions#new', :as => :new_user_session
+       post 'login' => 'devise/sessions#create', :as => :user_session
+       delete 'logout' => 'devise/sessions#destroy', :as => :destroy_user_session
+     end  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
