dabf2eceb0db1f9d5184da6ad0e93fc97b139a8f
github username <github email address>
2019-01-21 07:01:07 -0500

    add after sign in path for application controller and change omniauth controller name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:index]
 
+  def after_sign_in_path_for(resource)
+      request.env['omniauth.origin'] || root_path
+  end
   # private
   #   def current_user
   #     @current_user ||= User.find_by(id: session[:user_id])
--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,4 +1,4 @@
-class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def github
     @user = User.from_omniauth(request.env["omniauth.auth"])
     sign_in_and_redirect @user
