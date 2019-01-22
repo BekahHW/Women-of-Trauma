abaa3f0f3981225ace128b003a82d647da8910dc
github username <github email address>
2019-01-22 14:09:17 -0500

    Create relationships to therapists and user_therapists on user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :omniauthable, :omniauth_providers => [:github]
 
-  # has_many :therapists
-  # has_many :user_therapists, through: :therapists
+  has_many :therapists
+  has_many :user_therapists, through: :therapists
   has_many  :user_disorders
   has_many  :user_disorder_treatments, through: :user_disorders
   has_many :disorders, through: :user_disorders
@@ -15,18 +15,11 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
 
   def self.from_omniauth(auth)
-    # binding.pry
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      # user.provider = auth.provider
-      # user.uid = auth.uid
       user.name = auth.info.nickname
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
     end
   end
 
-end
-
-self.Location
- Therapist.where(:location).order
 end
