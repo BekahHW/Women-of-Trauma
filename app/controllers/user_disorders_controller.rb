a8ac0e858e4527f216b17a52db8c25227f8b8701
github username <github email address>
2019-01-22 05:18:14 -0500

    Add button to disorders show, update controller to comment out disorders new

--- app/controllers/user_disorders_controller.rb
+++ app/controllers/user_disorders_controller.rb
@@ -3,18 +3,22 @@ class UserDisordersController < ApplicationController
     @user_disorders = UserDisorder.limit(10)
 end
 
-#   def new
-#     @user_disorder = UserDisorder.new
-#   end
-#
-#   def create
-#     @user_disorder = UserDisorder.new(user_disorder_params)
-#      if @user_disorder.save
-#        redirect user_disorder_path(@user_disorder)
-#      else
-#        render :new
-#      end
-#   end
+  # def new
+  #   @user_disorder = current_user.disorder.user_disorders.build
+  #   @disorder = @user_disorder.disorder.last
+  #
+  # end
+  #
+  # def create
+  #   @user_disorder = current_user.user_disorders.build(user_disorder_params)
+  #   @user_disorder.disorder_id
+  #    if @user_disorder.save
+  #      redirect user_disorder_path(@user_disorder)
+  #    else
+  #      render :new
+  #    end
+  # end
+
 #
 #   def edit
 #     @user_disorder = UserDisorder.find(params[:id])
@@ -31,8 +35,8 @@ end
     @user_disorder = UserDisorder.find(params[:id])
   end
 #
-#   private
-#     def user_disorder_params
-#       params.require(:user_disorder).permit(:narrative)
-#     end
+  private
+    def user_disorder_params
+      params.require(:user_disorder).permit(:narrative, :user_id, :disorder_id)
+    end
 end
