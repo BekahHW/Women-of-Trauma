01b42d4fe4af3a7d7bb9ec75a17a496153a6b3bd
github username <github email address>
2019-01-24 10:20:59 -0500

    Eliminate commented out code on the UserTherapists controller

--- app/controllers/user_therapists_controller.rb
+++ app/controllers/user_therapists_controller.rb
@@ -1,28 +1,16 @@
 class UserTherapistsController < ApplicationController
   def new
     @user_therapist = UserTherapist.new
-    # @user_therapist.build(user_id: current_user)
   end
-
-  # def new
-  #  if params[:user_therapist_id] && !UserTherapist.exists?(params[:user_therapist_id])
-  #    redirect_to user_therapists_path, alert: "Therapist not found."
-  #  else
-  #    @user_therapist= UserTherapist.new(user_therapist_id: params[:user_therapist_id])
-  #  end
-  # end
-
 
   def create
     @user_therapist = current_user.user_therapists.new(user_therapist_params)
-    # @user_therapist = UserTherapist.new(user_therapist_params)
     if @user_therapist.save
       redirect_to user_user_therapist_path(current_user, @user_therapist)
     else
       render :new
     end
   end
-
 
   def show
     @user_therapist = UserTherapist.find_by(id: params[:id])
@@ -31,7 +19,6 @@ class UserTherapistsController < ApplicationController
   def index
     @user_therapists = UserTherapist.location
   end
-
 
   private
 
