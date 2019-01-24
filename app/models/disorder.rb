06e66f0b5566073626e9f1fc6f39cccdc17ac0f9
github username <github email address>
2019-01-24 11:05:29 -0500

    Clean up commented out code in Disorder  model

--- app/models/disorder.rb
+++ app/models/disorder.rb
@@ -4,25 +4,8 @@ class Disorder < ApplicationRecord
   has_many :users, through: :user_disorders
   has_many :treatments, through: :user_disorder_treatments
 
-
   accepts_nested_attributes_for :user_disorders
   validates :name, presence: true
-  validates :description, presence: true 
+  validates :description, presence: true
 
-
-
-# only creating a disorder if it's not already there
-  # def user_disorder_attributes=(narrative)
-  #   self.user_disorder = UserDisorder.create(:narrative)
-  # end
-  # #
-  #   def user_disorder_narrative
-  #     self.user_disorder ? self.user_disorder.narrative : nil
-  #   end
-
-  # def user_disorder_attributes=(user_disorder_attributes)
-  #  user_disorder_attributes.values.each do |user_disorder_attribute|
-  #    user_disorder = UserDisorder.find_or_create_by(user_disorder_attribute)
-  #    self.user_disorders << user_disorder
-  #  end
  end
