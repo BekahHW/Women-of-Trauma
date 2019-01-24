327e72613c57c746576b2b5299970fd599f0f8b7
github username <github email address>
2019-01-24 11:16:25 -0500

    Clean up commented out code in applications controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,12 +5,4 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
       request.env['omniauth.origin'] || root_path
   end
-  # private
-  #   def current_user
-  #     @current_user ||= User.find_by(id: session[:user_id])
-  #   end
-  #
-  #   def logged_in?
-  #     !!current_user
-  #   end
 end
