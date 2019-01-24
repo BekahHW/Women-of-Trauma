70f9a2e8e304226ddb3f0b4e889c6f708c50c32d
github username <github email address>
2019-01-24 11:02:41 -0500

    Clean up commented out code in omniauth controller

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -2,8 +2,6 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def github
     omniauth = request.env["omniauth.auth"]
     @user = User.from_omniauth(request.env["omniauth.auth"])
-    # binding.pry
     sign_in_and_redirect @user, :notice => "Welcome, #{@user.name}"
-    # redirect_to root_path, :notice => "Welcome, #{@user.name}"
   end
 end
