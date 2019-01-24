604f08a9c82c9b04d0c5e4b47e36b9cbd6d79186
github username <github email address>
2019-01-24 14:37:47 -0500

    Add errors_for to therapist and user_therapist

--- app/models/user_therapist.rb
+++ app/models/user_therapist.rb
@@ -1,6 +1,9 @@
 class UserTherapist < ApplicationRecord
   belongs_to :user
   before_save :capitalize_fields
+  validates :name, presence: true
+  validates :location, presence: true 
+
 
   scope :location, ->  { order(:location)}
 
