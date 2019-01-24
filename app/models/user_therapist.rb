993bef03b1df98c302ca00916acb898435ad438b
github username <github email address>
2019-01-24 11:04:33 -0500

    Clean up commented out code in UserTherapist model

--- app/models/user_therapist.rb
+++ app/models/user_therapist.rb
@@ -1,14 +1,10 @@
 class UserTherapist < ApplicationRecord
   belongs_to :user
-  # belongs_to :therapist
   before_save :capitalize_fields
 
-  # def self.location
-  #   UserTherapist.order(:location)
-  # end
   scope :location, ->  { order(:location)}
-# scope :therapist_name, -> {order(:name)}
-def capitalize_fields
- location.capitalize!
-end
+
+  def capitalize_fields
+   location.capitalize!
+  end
 end
