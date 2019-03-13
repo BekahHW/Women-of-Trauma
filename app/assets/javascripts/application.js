1d12b410fa800ced7bf65fec45c2c93cd6a081dd
github username <github email address>
2019-03-13 12:37:38 -0400

    Change order of application.js and eliminated jquery ujs

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
-//= require bootstrap-sprockets
 //= require rails-ujs
+//= require jquery
 //= require activestorage
+//= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
