b0c50bf9114353de540d95f22fff69c0140489dd
github username <github email address>
2019-03-13 11:20:54 -0400

    Add getUserDisorders() and class Disorder

--- app/assets/javascripts/disorders.js
+++ app/assets/javascripts/disorders.js
@@ -1,10 +1,14 @@
+$(function(){
+  console.log('it works`')
+})
+
 $(document).ready(function() {
   listenForClick()
 });
 
 // Add listener to a tags on disorders
 function listenForClick(){
-  $('.disorder').on('click', function(event) {
+  $('a').on('click', function(event) {
     event.preventDefault()
     getUserDisorders()
   })
@@ -12,5 +16,17 @@ function listenForClick(){
 
 
 function getUserDisorders(){
-  
+  $.ajax( {
+    url: 'http://localhost:3000/disorders',
+    method: 'get',
+    dataType: 'json',
+  }).done function(data)
+  console.log('the data is:', data)
+}
+
+class Disorder {
+  constuctor(obj){
+    this.id = obj.id
+    this.name = obj.name
+  }
 }
