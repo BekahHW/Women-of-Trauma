0118da4b036f70cdcd53e3de57bfa629d9a882ce
github username <github email address>
2019-03-13 10:54:17 -0400

    Add listenForClick fct in disorders.js

--- app/assets/javascripts/disorders.js
+++ app/assets/javascripts/disorders.js
@@ -1,8 +1,15 @@
 $(document).ready(function() {
-
+  listenForClick()
 });
 
 // Add listener to a tags on disorders
+function listenForClick(){
+  $('.disorder').on('click', function(event) {
+    event.preventDefault()
+    getUserDisorders()
+  })
+}
+
 
 function getUserDisorders(){
   
