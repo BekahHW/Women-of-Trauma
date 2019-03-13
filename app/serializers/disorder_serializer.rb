0d3e08aac4d81cceb78c2fe358d296a82dbba24d
github username <github email address>
2019-03-13 06:11:18 -0400

    Comment out custom methods in disorderserializer

--- app/serializers/disorder_serializer.rb
+++ app/serializers/disorder_serializer.rb
@@ -1,14 +1,14 @@
 class DisorderSerializer < ActiveModel::Serializer
-  attributes :id, :name, :user, :user_disorder
+  attributes :id, :name
   has_many :user_disorders
-  
-  def user
-    {user_id: self.object.user.id,
-     user_name: self.object.user.name}
-  end
 
-  def user_disorder
-    {user_disorder_id: self.object.user_disorder.id,
-     user_disorder_name: self.object.user_disorder.narrative}
-  end
+  # def user
+  #   {user_id: self.object.user.id,
+  #    user_name: self.object.user.name}
+  # end
+
+  # def user_disorder
+  #   {user_disorder_id: self.object.user_disorder.id,
+  #    user_disorder_name: self.object.user_disorder.narrative}
+  # end
 end
