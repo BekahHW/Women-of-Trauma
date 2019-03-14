5452ce91a4f612483f2741623e8cb17545020b67
github username <github email address>
2019-03-14 06:36:37 -0400

    Add descriptions to disorder seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,35 @@
-disorder_list = [
-   "Sexual Assault",
-   "Domestic Violence",
-   "Child Maltreatment",
-   "War-related Trauma",
-   "School and Community Violence",
-   "Medical Trauma",
-   "Traumatic Loss",
-   "Natural Disasters",
-]
+# disorder_list = [
+#    "Sexual Assault",
+#    "Domestic Violence",
+#    "Child Maltreatment",
+#    "War-related Trauma",
+#    "School and Community Violence",
+#    "Medical Trauma",
+#    "Traumatic Loss",
+#    "Natural Disasters",
+# ]
+#
+# disorder_list.each do |name|
+#   Disorder.create( name: name )
+# end
 
-disorder_list.each do |name|
-  Disorder.create( name: name )
-end
+Disorder.create(name: 'Natural Disasters', description: 'Any natural catastrophe (e.g., tornadoes, hurricanes, and earthquakes) that causes enough damage that local, state, or federal agencies and disaster relief organizations are called into action.')
+
+Disorder.create(name: 'Traumatic Loss', description: 'Traumatic loss or grief can occur following a death of someone important to a child or adult. The death is typically sudden and unexpected.')
+
+Disorder.create(name: 'Medical Trauma', description: 'Children may show traumatic reactions to medical conditions, invasive medical procedures, or treatments that are frightening or cause pain, injury, and/or serious illness.')
+
+Disorder.create(name: 'School and Community Violence', description: 'School and community violence include predatory violence or personal conflicts between people who are not family members (e.g., shootings, rape, robbery).')
+
+
+Disorder.create(name: "War-related Trauma", description: "Refugee and warzone trauma. Terrorism. Combat-related trauma.")
+
+Disorder.create(name: Sexual Assault, description: "Sexual assault involves any unwanted and involuntary sexual behavior towards a person. The victim is forced or coerced engage in an act against their will in a non-consensual setting. Sexual assault can include rape, groping, forced kissing, and any other sort of harassment or abuse in a sexual context.")
+
+Disorder.create(name: Domestic Violence, description: "Domestic violence is classified as actual or threatened physical violence, sexual violence, and/or emotional abuse between adults in an intimate relationship.")
+
+Disorder.create(name: Child Maltreatment, description: "Neglect. Physical abuse. Mental abuse. Maltreatment. Sexual Abuse." )
+
 
 User.create(name: Bekah, location: Ohio)
 User.create(name: Jesse, location: WV)
