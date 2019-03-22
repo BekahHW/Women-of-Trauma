c6412206b684748a39cac574371c96c2f25ea147
github username <github email address>
2019-03-22 10:56:45 -0400

    Cut commented out code from disorders.js

--- app/assets/javascripts/disorders.js
+++ app/assets/javascripts/disorders.js
@@ -14,17 +14,6 @@ $(function() {
   })
   listenForClick()
 
-
-//   $(function() {
-//     $.ajax( {
-//       url: 'http://localhost:3000/users/welcome.json',
-//       method: 'get',
-//       dataType: 'json',
-//     }).done(function(data){
-//       userId = data.data.id
-//     })
-// })
-  //
   fetch('http://localhost:3000/users/welcome.json')
   .then(data => data.json())
   .then(data => {
@@ -34,7 +23,6 @@ $(function() {
 
 init()
 
-// Add listener to a tags on disorders
 function listenForClick(){
   $('.disorder_link').on('click', function(event) {
     event.preventDefault()
@@ -46,8 +34,7 @@ function listenForClick(){
 
 function getDisorderShow(id){
   let newDisorder = new Disorder(globalDataStore[id - 1])
-  // debugger
-  // let newDisorderHTML = newDisorder.description
+
   let newDisorderHTML = newDisorder.disorderHTML()
   $(`#description${newDisorder.id}`).html(newDisorderHTML).append('<p><button type="button" class="btn btn-primary" id="addStories">Add Story</button></p>')
   addStoryClick(newDisorder.id)
@@ -82,69 +69,12 @@ let storyForm = (`
 
 }
 
-
-// const handleForm = () => {
-//   $(`.storyFormBtn`).click(function() {
-//     event.preventDefault()
-//
-//     textareaVal = $(this).parent('form').children('.form-group').children('textarea').val()
-//
-//     const formVal = {
-//         narrative: textareaVal,
-//         user_disorder_id: '1',
-//         user_id: userId,
-//         disorder_id: $(this).attr('disorder_id')
-//
-//     }
-//     var formData = new FormData();
-//     console.log("In post request")
-//     formData.append('user_disorder',  JSON.stringify(formVal))
-//
-//     const token = $('meta[name="csrf-token"]').attr('content')
-//     fetch(`http://localhost:3000/user_disorders`
-//       , {
-//     method: 'POST',
-//     credentials: 'include',
-//     headers: {
-//       // 'X-Requested-With': 'XMLHttpRequest',
-//       'X-CSRF-Token': token,
-//       //
-//       'Content-Type': 'application/json',
-//       //         'Accept': 'application/json'
-//
-//   // 'X-CSRF-Token': '<%= form_authenticity_token.to_s %>'
-//
-//     },
-//
-//     body: formData}
-//   )
-//
-//   .then(response => response.json())
-// .catch(error => console.error('Error:', error))
-// .then(response => console.log('Success:', JSON.stringify(response)));
-//
-//   })
-// }
-
-
-
-//
-// function getStoriesShow(id){
-//   console.log("Stories go here")
-//   // debugger
-//   //
-//   // $(`#description${id}`).text(disorderHTML()).append('<p><button type="button" class="btn btn-primary" id="seeStories">New Story</button></p>')
-// }
-
-
-
 class Disorder {
   constructor(obj){
     this.id = obj.id
     this.name = obj.attributes.name
     this.description = obj.attributes.description
     this.narrative = obj.attributes.user_disorders
-// obj.attributes.user_disorders[0].narrative
   }
 }
 
